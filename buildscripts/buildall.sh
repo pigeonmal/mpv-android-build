bc02d99a51437de73ec2b2c50bd576ffa9174c02
pigeonmal <eliastafnakji1@gmail.com>
2025-01-31 23:47:09 +0100

    First test

--- buildscripts/buildall.sh
+++ buildscripts/buildall.sh
@@ -5,9 +5,8 @@ cd "$( dirname "${BASH_SOURCE[0]}" )"
 
 cleanbuild=0
 nodeps=0
-clang=1
-target=mpv-android
-arch=armv7l
+target=mpv
+archs=(armv7l arm64 x86 x86_64)
 
 getdeps () {
 	varname="dep_${1//-/_}[*]"
@@ -16,7 +15,6 @@ getdeps () {
 
 loadarch () {
 	unset CC CXX CPATH LIBRARY_PATH C_INCLUDE_PATH CPLUS_INCLUDE_PATH
-	unset CFLAGS CXXFLAGS CPPFLAGS LDFLAGS
 
 	local apilvl=21
 	# ndk_triple: what the toolchain actually is
@@ -25,12 +23,12 @@ loadarch () {
 		export ndk_suffix=
 		export ndk_triple=arm-linux-androideabi
 		cc_triple=armv7a-linux-androideabi$apilvl
-		prefix_name=armv7l
+		prefix_name=armeabi-v7a
 	elif [ "$1" == "arm64" ]; then
 		export ndk_suffix=-arm64
 		export ndk_triple=aarch64-linux-android
 		cc_triple=$ndk_triple$apilvl
-		prefix_name=arm64
+		prefix_name=arm64-v8a
 	elif [ "$1" == "x86" ]; then
 		export ndk_suffix=-x86
 		export ndk_triple=i686-linux-android
@@ -42,17 +40,18 @@ loadarch () {
 		cc_triple=$ndk_triple$apilvl
 		prefix_name=x86_64
 	else
-		echo "Invalid architecture" >&2
+		echo "Invalid architecture"
 		exit 1
 	fi
 	export prefix_dir="$PWD/prefix/$prefix_name"
-	if [ $clang -eq 1 ]; then
-		export CC=$cc_triple-clang
-		export CXX=$cc_triple-clang++
+	export native_dir="$PWD/../libmpv/src/main/jniLibs/$prefix_name"
+	export CC=$cc_triple-clang
+	if [[ "$1" == arm* ]]; then
+		export AS="$CC"
 	else
-		export CC=$cc_triple-gcc
-		export CXX=$cc_triple-g++
+		export AS="nasm"
 	fi
+	export CXX=$cc_triple-clang++
 	export LDFLAGS="-Wl,-O1,--icf=safe -Wl,-z,max-page-size=16384"
 	export AR=llvm-ar
 	export RANLIB=llvm-ranlib
@@ -66,29 +65,26 @@ setup_prefix () {
 		ln -s . "$prefix_dir/local"
 	fi
 
+	if [ ! -d "$native_dir" ]; then
+		mkdir -p "$native_dir"
+	fi
+
 	local cpu_family=${ndk_triple%%-*}
 	[ "$cpu_family" == "i686" ] && cpu_family=x86
 
-	if ! command -v pkg-config >/dev/null; then
-		echo "pkg-config not provided!"
-		return 1
-	fi
-
 	# meson wants to be spoonfed this file, so create it ahead of time
 	# also define: release build, static libs and no source downloads at runtime(!!!)
-	cat >"$prefix_dir/crossfile.tmp" <<CROSSFILE
+	cat >"$prefix_dir/crossfile.txt" <<CROSSFILE
 [built-in options]
 buildtype = 'release'
 default_library = 'static'
 wrap_mode = 'nodownload'
-prefix = '/usr/local'
 [binaries]
 c = '$CC'
 cpp = '$CXX'
 ar = 'llvm-ar'
 nm = 'llvm-nm'
 strip = 'llvm-strip'
-pkgconfig = 'pkg-config'
 pkg-config = 'pkg-config'
 [host_machine]
 system = 'android'
@@ -96,16 +92,10 @@ cpu_family = '$cpu_family'
 cpu = '${CC%%-*}'
 endian = 'little'
 CROSSFILE
-	# also avoid rewriting it needlessly
-	if cmp -s "$prefix_dir"/crossfile.{tmp,txt}; then
-		rm "$prefix_dir/crossfile.tmp"
-	else
-		mv "$prefix_dir"/crossfile.{tmp,txt}
-	fi
 }
 
 build () {
-	if [ $1 != "mpv-android" ] && [ ! -d deps/$1 ]; then
+	if [ ! -d deps/$1 ]; then
 		printf >&2 '\e[1;31m%s\e[m\n' "Target $1 not found"
 		return 1
 	fi
@@ -117,27 +107,23 @@ build () {
 			build $dep
 		done
 	fi
+
 	printf >&2 '\e[1;34m%s\e[m\n' "Building $1..."
-	if [ "$1" == "mpv-android" ]; then
-		pushd ..
-		BUILDSCRIPT=buildscripts/scripts/$1.sh
-	else
-		pushd deps/$1
-		BUILDSCRIPT=../../scripts/$1.sh
-	fi
+	pushd deps/$1
+	BUILDSCRIPT=../../scripts/$1.sh
+ 	sudo chmod +x $BUILDSCRIPT
 	[ $cleanbuild -eq 1 ] && $BUILDSCRIPT clean
-	$BUILDSCRIPT build
-	popd
+    $BUILDSCRIPT build
+    popd
 }
 
 usage () {
 	printf '%s\n' \
-		"Usage: buildall.sh [options] [target]" \
+		"Usage: build.sh [options] [target]" \
 		"Builds the specified target (default: $target)" \
 		"-n             Do not build dependencies" \
 		"--clean        Clean build dirs before compiling" \
-		"--gcc          Use gcc compiler (unsupported!)" \
-		"--arch <arch>  Build for specified architecture (default: $arch; supported: armv7l, arm64, x86, x86_64)"
+		"--arch <arch>  Build for specified architecture (supported: armv7l, arm64, x86, x86_64)"
 	exit 0
 }
 
@@ -149,19 +135,12 @@ while [ $# -gt 0 ]; do
 		-n|--no-deps)
 		nodeps=1
 		;;
-		--gcc)
-		clang=0
-		;;
 		--arch)
 		shift
 		arch=$1
 		;;
 		-h|--help)
 		usage
-		;;
-		-*)
-		echo "Unknown flag $1" >&2
-		exit 1
 		;;
 		*)
 		target=$1
@@ -170,11 +149,16 @@ while [ $# -gt 0 ]; do
 	shift
 done
 
-loadarch $arch
-setup_prefix
-build $target
-
-[ "$target" == "mpv-android" ] && \
-	ls -lh ../app/build/outputs/apk/{default,api29}/*/*.apk
+if [ -z $arch ]; then
+  for arch in ${archs[@]}; do
+    loadarch $arch
+    setup_prefix
+    build $target
+  done
+else
+  loadarch $arch
+  setup_prefix
+  build $target
+fi
 
 exit 0
